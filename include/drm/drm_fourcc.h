f421c9c8c4b8fe48d9e6ef43910e98569c94a4b2
Haihua Hu <jared.hu@nxp.com>
2019-11-22 16:58:50 +0800

    add NV12_10LE40 and new HDR10 metadata struct for L5.4
    L5.4 use new 10bit packaged NV12 format and also the HDR10
    metadata struct is changed by upstream. Add new struct here
    to pass building on L5.4
    
    Signed-off-by: Haihua Hu <jared.hu@nxp.com>

--- include/drm/drm_fourcc.h
+++ include/drm/drm_fourcc.h
@@ -241,6 +241,14 @@ extern "C" {
 #define DRM_FORMAT_NV24		fourcc_code('N', 'V', '2', '4') /* non-subsampled Cr:Cb plane */
 #define DRM_FORMAT_NV42		fourcc_code('N', 'V', '4', '2') /* non-subsampled Cb:Cr plane */
 #define DRM_FORMAT_P010		fourcc_code('P', '0', '1', '0') /* 2x2 subsampled Cr:Cb plane, 10-bit per channel */
+/*
+ * A fully packed  2 plane YCbCr
+ * Y1 0-9, Y2 10-19, Y3 20-29, Y4 20-39
+ * ....
+ * U1V1: 0-19, U2V2: 20-39
+ * since kernel 5.4
+ */
+#define DRM_FORMAT_NV12_10LE40	fourcc_code('R', 'K', '2', '0') /* 2x2 subsampled Cr:Cb plane */
 
 /*
  * 2 plane YCbCr MSB aligned
