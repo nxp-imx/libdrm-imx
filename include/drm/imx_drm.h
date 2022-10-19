acef00324c2a783043279aae63f86b01f985bfb1
Jessie Hao <juan.hao@nxp.com>
2022-10-19 13:59:06 +0800

    MA-20797 Update imx_drm header to enable fence support in DRM blitter API
    Update imx_drm.h to align with the below patch in kernel:
    43acef5d23d5 MGS-6488-4 gpu: imx: dpu-blit: Add fence support in DRM blitter API
    Use the below script under $ANDROID_BUILD_TOP to generate the header file
    automatically:
    ./device/nxp/common/tools/update_kerenl_header.sh
    
    Signed-off-by: Jessie Hao <juan.hao@nxp.com>
    (cherry picked from commit c6cf6625912178d88616ae650bf1e58f5ee51b0b)

--- include/drm/imx_drm.h
+++ include/drm/imx_drm.h
@@ -21,46 +21,37 @@
 #include "drm.h"
 #ifdef __cplusplus
 extern "C" {
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 #endif
 struct drm_imx_dpu_frame_info {
   __u32 width;
   __u32 height;
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
   __u32 x_offset;
   __u32 y_offset;
   __u32 stride;
   __u32 format;
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
   __u64 modifier;
   __u64 baddr;
   __u64 uv_addr;
 };
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 #define DRM_IMX_DPU_SET_CMDLIST 0x00
 #define DRM_IMX_DPU_WAIT 0x01
 #define DRM_IMX_DPU_GET_PARAM 0x02
 #define DRM_IOCTL_IMX_DPU_SET_CMDLIST DRM_IOWR(DRM_COMMAND_BASE + DRM_IMX_DPU_SET_CMDLIST, struct drm_imx_dpu_set_cmdlist)
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 #define DRM_IOCTL_IMX_DPU_WAIT DRM_IOWR(DRM_COMMAND_BASE + DRM_IMX_DPU_WAIT, struct drm_imx_dpu_wait)
 #define DRM_IOCTL_IMX_DPU_GET_PARAM DRM_IOWR(DRM_COMMAND_BASE + DRM_IMX_DPU_GET_PARAM, enum drm_imx_dpu_param)
 struct drm_imx_dpu_set_cmdlist {
   __u64 cmd;
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
   __u32 cmd_nr;
   __u64 user_data;
 };
 struct drm_imx_dpu_wait {
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
   __u64 user_data;
 };
 enum drm_imx_dpu_param {
   DRM_IMX_MAX_DPUS,
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+  DRM_IMX_GET_FENCE,
 };
 #ifdef __cplusplus
 }
 #endif
 #endif
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-
