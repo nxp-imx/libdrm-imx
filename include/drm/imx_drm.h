d9907a8ca70f2dda44cbb359216c7d20ed829031
Jessie Hao <juan.hao@nxp.com>
2025-10-13 15:51:17 +0800

    MA-24133 Update imx_drm header to support dmabuf fd in dpu95-bliteng.
    Update imx_drm.h to align with the below patch in kernel 6.12:
    
    1752ab8e0026 MGS-8313-2 drm/imx: dpu95-bliteng: support dmabuf fd in
    src/dst buffer
    
    Use the below script under $ANDROID_BUILD_TOP to generate the header
    file automatically:
    ./device/nxp/common/tools/update_kerenl_header.sh
    
    Signed-off-by: Jessie Hao <juan.hao@nxp.com>

--- include/drm/imx_drm.h
+++ include/drm/imx_drm.h
@@ -1,21 +1,9 @@
-/****************************************************************************
- ****************************************************************************
- ***
- ***   This header was automatically generated from a Linux kernel header
- ***   of the same name, to make information necessary for userspace to
- ***   call into the kernel available to libc.  It contains only constants,
- ***   structures, and macros generated from the original header, and thus,
- ***   contains no copyrightable information.
- ***
- ***   To edit the content of this header, modify the corresponding
- ***   source file (e.g. under external/kernel-headers/original/) then
- ***   run bionic/libc/kernel/tools/update_all.py
- ***
- ***   Any manual change here will be lost the next time this script will
- ***   be run. You've been warned!
- ***
- ****************************************************************************
- ****************************************************************************/
+/*
+ * This file is auto-generated. Modifications will be lost.
+ *
+ * See https://android.googlesource.com/platform/bionic/+/master/libc/kernel/
+ * for more information.
+ */
 #ifndef _UAPI_IMX_DRM_H_
 #define _UAPI_IMX_DRM_H_
 #include "drm.h"
@@ -32,6 +20,12 @@ struct drm_imx_dpu_frame_info {
   __u64 modifier;
   __u64 baddr;
   __u64 uv_addr;
+};
+struct drm_imx_dpu_frame_plane_info {
+  int src_plane_fd[3];
+  __u32 src_plane_offset[3];
+  int dst_plane_fd[3];
+  __u32 dst_plane_offset[3];
 };
 #define DRM_IMX_DPU_SET_CMDLIST 0x00
 #define DRM_IMX_DPU_WAIT 0x01
